93062f64753120a22526dab32575518ec299d96d
Villiam <Vjacob19@student.aau.dk>
2022-04-01 10:15:30 +0200

    test

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    install_requires=[
+        'numpy',
+        'pandas',
+        'prettytable',
+        'scipy',
+        'scikit-learn',
+        'ventmap',
+        "ventmode"
+    ],
+    dependency_links=[
+        "git+https://github.com/hahnicity/ventmode"
+    ]
+)
